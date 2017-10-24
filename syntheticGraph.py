76121d9459c08fce17b43ffc5ffe2724baa98685
fubao <freadream@gmail.com>
2017-10-24 12:04:09 -0400

    add_file

---
+++ syntheticGraph.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 24 11:41:47 2017
+
+@author: fubao
+"""
+
+import networkx as nx
+import collections
+
+import matplotlib.pyplot as plt
+from networkx.readwrite import json_graph;
+import json
+import webbrowser
+import sys
+sys.path.append("../")
+
+from hierarchicalQueryPython.graphCommon import readCiscoDataGraph
+
+
+#plot graph functions here;  plot locally with matplotlib; plot in D3...
+class syntheticGraph(object):
+    
+    def __init__(self):
+        pass
+    
