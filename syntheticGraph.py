20ccbc313882206da5322e813fbc5ee039a4d85f
fubao <freadream@gmail.com>
2017-10-25 13:42:18 -0400

    update

--- syntheticGraph.py
+++ syntheticGraph.py
@@ -32,23 +32,24 @@ from hierarchicalQueryPython.CommonFiles.commons import writeListToFileWriterTsv
 from enum import Enum
 
 class GRAPHNODETYPE(Enum):
-    TYPE0HIER = 0             #hierarchical nodes with subcategory, category     20%
-    TYPE1HIER = TYPE0HIER+1   #hierarchical nodes with subcategory, category      20%
+    TYPE0HIER = 0             #hierarchical nodes with subcategory, category     30%
+    TYPE1HIER = TYPE0HIER+1   #hierarchical nodes with subcategory, category      30%
     TYPE0INHERIT = TYPE0HIER + 2   #node that inheritance among hierarchical nodes 10%
     TYPE1INHERIT = TYPE0HIER + 3   #node that inheritance among hierarchical nodes 10%                               
-    TYPE0GENERIC = TYPE0HIER + 4   # 10%
+    TYPE0GENERIC = TYPE0HIER + 4   #5%
     TYPE1GENERIC = TYPE0HIER + 5   #10%
-    TYPE2GENERIC = TYPE0HIER + 6   #20%
+    TYPE2GENERIC = TYPE0HIER + 6   #10%
     
     
 class syntheticGraph(object):
     
     def __init__(self):
-        self.totalNodeNumber  = 1000    #10000000      #10million
+        self.totalNodeNumber  = 1000000   #1000    #10000000      #10million
        # self.totalEdgeNumber  = 50000000      #50million;  node number decided by edge number
         self.nodeIdToTypeMap = defaultdict()         #node Id to node type Id
         self.nodeIdToNameMap = defaultdict()         #node Id to node name
 
+        self.nodeTypeNumberPartioLst = [0.3, 0.3, 0.1, 0.1, 0.1, 0.05, 0.05]
 
     def judgeEdgeHierLevel(self, nodeIdSrc, nodeIdDst):
         '''
@@ -76,25 +77,27 @@ class syntheticGraph(object):
                 nodeId = startNodeId + j
                 nodeName = nodeTypeLst[i].name +"_" + str(nodeId) + "_+++" + str(nodeTypeLst[i].value)
                 
-                #write into list
-                listNodeInfo.append([nodeName, nodeId])
-                self.nodeIdToTypeMap[nodeId] = nodeName
-                self.nodeIdToNameMap[nodeId] = nodeTypeLst[i].value
-                
+                if nodeId <= self.totalNodeNumber:
+                    #write into list
+                    listNodeInfo.append([nodeName, nodeId])
+                    self.nodeIdToTypeMap[nodeId] = nodeTypeLst[i].value 
+                    self.nodeIdToNameMap[nodeId] = nodeName
                 
             startNodeId = startNodeId + nodeNumber
         return startNodeId, listNodeInfo
 
 
-    def generateEdgeList(self, maximumDegree):
+    def generateEdgeListVersion1(self, maximumDegree):
         '''
+        version 1: total random of hierarchical level 
         generate edge for nodes in the graph
         '''
         #edge hierarchical level same, higher, lower
         
         edgeList = []
         hierarchiLevelList = ['same', 'higher', 'lower']
-        listNodes = self.nodeIdToTypeMap.keys()
+        listNodes = list(self.nodeIdToTypeMap.keys())
+        pairEdgeMap = defaultdict()               #used for checking whether pair edge is created before
         for nodeId in listNodes:
             #generate edge
             number = randint(0, maximumDegree)
@@ -105,14 +108,33 @@ class syntheticGraph(object):
                     #construct edge
                     if nodeId != selectNodeId:
                         if self.judgeEdgeHierLevel(nodeId, selectNodeId):     #hierarchical node
+                            #print ("enter here: ", self.nodeIdToTypeMap[nodeId], self.nodeIdToTypeMap[selectNodeId])
                             #select hierarchical node level 
                             hierLevel = choice(hierarchiLevelList)
-                            edgeList.append([nodeId, selectNodeId, hierLevel])
+                            if (nodeId, selectNodeId) not in pairEdgeMap:
+                                edgeList.append([nodeId, selectNodeId, hierLevel])
+                                if hierLevel == "higher":
+                                    edgeList.append([selectNodeId, nodeId, "lower"])
+                                elif hierLevel == "lower":
+                                    edgeList.append([selectNodeId, nodeId, "higher"])
+                                else:
+                                    edgeList.append([selectNodeId, nodeId, hierLevel])
+
+                                pairEdgeMap[(nodeId, selectNodeId)] = 1
+                                pairEdgeMap[(selectNodeId, nodeId)] = 1
                         else:
+                            #print ("enter here2 : ", self.nodeIdToTypeMap[nodeId], self.nodeIdToTypeMap[selectNodeId])
                             hierLevel = hierarchiLevelList[0]
-                            edgeList.append([nodeId, selectNodeId, hierLevel])
+                            if (nodeId, selectNodeId) not in pairEdgeMap:
+                                edgeList.append([nodeId, selectNodeId, hierLevel])
+                                edgeList.append([selectNodeId, nodeId, hierLevel])
+                                pairEdgeMap[(nodeId, selectNodeId)] = 1
+                                pairEdgeMap[(selectNodeId, nodeId)] = 1
+                                
         return edgeList
         
+    
+    
     def generateAllNodeInfo(self, outFileNodeInfo, outFileEdgeList):
         '''
         generate all node info file
@@ -122,33 +144,36 @@ class syntheticGraph(object):
         #generate hierarchical node info, label + ID
         startNodeId = 1
         nodeTypeLst = [GRAPHNODETYPE.TYPE0HIER, GRAPHNODETYPE.TYPE1HIER]
-        nodesNumberLst = [int(self.totalNodeNumber * 0.2), int(self.totalNodeNumber * 0.2)]                        #20%
+        nodesNumberLst = [int(self.totalNodeNumber * self.nodeTypeNumberPartioLst[0]), int(self.totalNodeNumber * self.nodeTypeNumberPartioLst[1])]                        #30%
         startNodeId, listNodeInfo = self.generateHierNode(startNodeId, nodeTypeLst, nodesNumberLst, listNodeInfo)
         print ("generateAllNodeInfo listNodeInfo 1:  ", len(listNodeInfo))
         
         # generate info of node that inherited 
         nodeTypeLst = [GRAPHNODETYPE.TYPE0INHERIT, GRAPHNODETYPE.TYPE1INHERIT]
-        nodesNumberLst = [int(self.totalNodeNumber * 0.1), int(self.totalNodeNumber * 0.1)]                        #20%
+        nodesNumberLst = [int(self.totalNodeNumber * self.nodeTypeNumberPartioLst[2]), int(self.totalNodeNumber * self.nodeTypeNumberPartioLst[3])]                        #20%
         startNodeId, listNodeInfo = self.generateHierNode(startNodeId, nodeTypeLst, nodesNumberLst, listNodeInfo)        
         print ("generateAllNodeInfo listNodeInfo  2: ", len(listNodeInfo))
 
                 
         # generate info of node that inherited 
         nodeTypeLst = [GRAPHNODETYPE.TYPE0GENERIC, GRAPHNODETYPE.TYPE1GENERIC, GRAPHNODETYPE.TYPE2GENERIC]
-        nodesNumberLst = [int(self.totalNodeNumber * 0.1), int(self.totalNodeNumber * 0.1), int(self.totalNodeNumber * 0.2)]                        #20%
+        nodesNumberLst = [int(self.totalNodeNumber * self.nodeTypeNumberPartioLst[4]), int(self.totalNodeNumber * self.nodeTypeNumberPartioLst[5]), int(self.totalNodeNumber * self.nodeTypeNumberPartioLst[6])]                        #20%
         startNodeId, listNodeInfo = self.generateHierNode(startNodeId, nodeTypeLst, nodesNumberLst, listNodeInfo)
         print ("generateAllNodeInfo listNodeInfo  3: ", len(listNodeInfo))
           
         #get edge list file
-        edgeList = generateEdgeList(outFileNodeInfo, outFileEdgeList)
+        maximumDegree = 10
+        edgeList = self.generateEdgeListVersion1(maximumDegree)
+        
+        print ("generateAllNodeInfo edgeList  : ", len(edgeList))
         
         #write into file
-        os.remove(outFile) if os.path.exists(outFile) else None
-
-        writeListToFileWriterTsv(outFile, listNodeInfo, "\t")
-        
+        os.remove(outFileNodeInfo) if os.path.exists(outFileNodeInfo) else None
+        os.remove(outFileEdgeList) if os.path.exists(outFileEdgeList) else None
+        writeListToFileWriterTsv(outFileNodeInfo, listNodeInfo, "\t")
+        writeListToFileWriterTsv(outFileEdgeList, edgeList, "\t")
                 
-
+        
         
           
 if __name__ == "__main__":
